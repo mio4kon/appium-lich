31cf13907a483721284876c32e4a15d298ce9352
mio4kon <mio4kon@sina.com>
2017-04-18 14:45:48 +0800

    add shell args

--- run.py
+++ run.py
@@ -7,6 +7,19 @@ import pytest
 from utils.environment import Environment
 from utils.shell import Shell
 from utils import L
+import sys
+
+"""
+run all case:
+    python3 run.py
+
+run one module case:
+    python3 run.py test/test_home.py
+
+run case with key word:
+    python3 run.py -k <keyword>
+
+"""
 
 if __name__ == '__main__':
     env = Environment()
@@ -14,7 +27,8 @@ if __name__ == '__main__':
     html_report_path = env.get_environment_info().html_report
     # 开始测试
     args = ['-s', '-q', '--alluredir', xml_report_path]
-    pytest.main(args)
+    self_args = sys.argv[1:]
+    pytest.main(args + self_args)
     # 生成html测试报告
     cmd = 'allure generate %s -o %s' % (xml_report_path, html_report_path)
     try:
